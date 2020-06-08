a649a3da30c20cf0a3c50c6651be55d1f3396494
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 01:04:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc (array)
+  array.sort { |a, b| a <=> b }
+end 
 
+def sort_array_desc (array)
+  array.sort { |a, b| b <=> a }
+end 
+
+def sort_array_char_count (array) 
+  array.sort { |a, b|  a.length <=> b.length }
+end 
+
+def swap_elements (array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end 
+
+def reverse_array (array)
+  array.reverse 
+end 
+
+def kesha_maker (array)
+  new_array = []
+  
+  while index < array.length 
+    array.each { |name|
+      name.insert 2, "$"}
+  end
+   
+  
+  new_array
+end 
